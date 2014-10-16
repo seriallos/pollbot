e3e22485fda6c904ff25771572d0051b8f696dfb
Dave Hendler <sollaires@gmail.com>
2014-10-16 16:46:07 -0500

    Specify HTTP port, redis brain, and npm install in run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+export PORT=8082
+
+export REDIS_URL=redis://localhost:6379/pollbot
+
 export HUBOT_IRC_SERVER=irc.coldfront.net
 export HUBOT_IRC_ROOMS=#adhocracy
 export HUBOT_IRC_NICK=PollBot
 export HUBOT_IRC_UNFLOOD=true
+
+npm install
 
 bin/hubot -a irc --name PollBot
